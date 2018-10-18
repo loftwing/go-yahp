b2002fb132e03256a4f325530fda268f0d4976a8
Chance <albatross@loftwing.net>
2018-10-18 11:59:25 -0500

    update

--- yahp/sensor/sensor_manager.go
+++ yahp/sensor/sensor_manager.go
@@ -1,78 +1,43 @@
 package sensor
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os/exec"
-	"time"
+	`log`
+
+	`golang.org/x/sys/windows/registry`
 )
 
-type Port struct {
-	Port    int
-	Started bool
+type SensorManager struct {
+	ConfigServerAddr string
+
+	LogChan  chan string
+
+	Pm *PortManager
+	Mm *MessageManager
 }
 
-type PortManager struct {
-	Mq      chan string
-	LogChan chan<- string
-	Ports   []*Port
-}
-
-func NewPortManager(lc chan<- string, port ...int) *PortManager {
-	var ports []*Port
-	for _, v := range port {
-		sensor := &Port{Port: v, Started: false}
-		ports = append(ports, sensor)
-	}
-	return &PortManager{
-		Mq:      make(chan string, 512),
-		Ports:   ports,
-		LogChan: lc,
-	}
-}
-
-func (pm *PortManager) StartAll() {
-	for _, v := range pm.Ports {
-		go v.start(pm.Mq)
-	}
-	pm.StartMonitor()
-}
-
-func startListener(ctx context.Context, port string) (string, error) {
-	cmd := exec.CommandContext(ctx, "port.exe", port)
-	out, err := cmd.Output()
+func NewSensorManager() (*SensorManager) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\yahp`, registry.QUERY_VALUE)
 	if err != nil {
-		return "", err
+		log.Panic("REGISTRY: yahp key could not be opened.")
 	}
-	return string(out), nil
+	defer k.Close()
+
+	s, _, err := k.GetStringValue("ConfigServerAddr")
+	if err != nil {
+		log.Panic("REGISTRY: Value ConfigServerAddr could not be read.")
+	}
+
+	return &SensorManager{
+		ConfigServerAddr: s,
+		Pm: nil,
+		Mm: nil,
+	}
 }
 
-func (pm *PortManager) StartMonitor() {
-	go func(lc chan<- string) {
-		for {
-			for _, v := range pm.Ports {
-				lc <- fmt.Sprintf("%d:%t", v.Port, v.Started)
-			}
-			time.Sleep(time.Second * 150)
-		}
-	}(pm.LogChan)
+func getConfig() {
+
 }
 
-func (s *Port) start(mc chan<- string) {
-	for {
-		s.Started = true
-		port := fmt.Sprintf("%d", s.Port)
-		out, err := startListener(context.Background(), port)
-		if err != nil {
-			log.Println("sensor exec err: ", err)
-			s.Started = false
-		}
+func (sm *SensorManager) Run() {
 
-		// send output through to channel of sgroup
-		mc <- out
-
-		// wait 1 min before re-opening the port, disabled while developing
-		time.Sleep(time.Minute)
-	}
 }
